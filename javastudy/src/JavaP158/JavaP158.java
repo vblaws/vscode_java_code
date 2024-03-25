8b4e4dc8660c2b4e5022f5d9f5c6afa1605fac0e
vblaws <H3239498031@gmail.com>
2024-03-25 20:02:23 +0800

    API-Runtime

---
+++ javastudy/src/JavaP158/JavaP158.java
@@ -0,0 +1,10 @@
+package JavaP158;
+
+public class JavaP158 {
+    public static void main(String[] args) {
+        // API-Runtime
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println(runtime.maxMemory());
+    }
+
+}
