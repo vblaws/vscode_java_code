7b9452b1a7050cda19e8c9b29a47e91dca77e1f2
vblaws <H3239498031@gmail.com>
2024-03-26 10:05:32 +0800

    3/26 10:05

--- javastudy/src/JavaP158/JavaP58DemoJFame.java
+++ javastudy/src/JavaP158/JavaP58DemoJFame.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
@@ -12,7 +13,7 @@ import javax.swing.WindowConstants;
 public class JavaP58DemoJFame extends JFrame implements ActionListener {
     // 将按钮对象定义到成员位置,以便为了调用
     JButton yesBut = new JButton("帅炸了");
-    JButton midBut = new JButton("一般般");
+    JButton midBut = new JButton("我错了");
     JButton badBut = new JButton("好丑");
     // 获取系统runtime对象
 
@@ -42,7 +43,7 @@ public class JavaP58DemoJFame extends JFrame implements ActionListener {
         getContentPane().removeAll();
         JLabel test = new JLabel("你觉得我帅吗");
 
-        System.out.println(test.getText());
+        // System.out.println(test.getText());
         test.setBounds(200, 150, 300, 50);
         // 设置按钮尺寸和位置
         yesBut.setBounds(100, 250, 300, 50);
@@ -56,26 +57,56 @@ public class JavaP58DemoJFame extends JFrame implements ActionListener {
         getContentPane().add(yesBut);
         getContentPane().add(midBut);
         getContentPane().add(badBut);
+        // 添加提示语
         getContentPane().add(test);
         getContentPane().repaint();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // 获取是哪个对象
         Object object = e.getSource();
         if (object == yesBut) {
-            System.out.println("谢谢夸奖");
+            showDiwlog("谢谢夸奖");
         } else if (object == midBut) {
-            System.out.println("中肯的评价");
-        } else if (object == badBut) {
-            System.out.println("你完了");
+            showDiwlog("我错了");
             try {
+                // 执行系统cmd程序
+                Runtime.getRuntime().exec(new String[] { "shutdown", "-a" });
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+        } else if (object == badBut) {
+            showDiwlog("你完了");
+            try {
+                // 执行系统cmd程序
                 Runtime.getRuntime().exec(new String[] { "shutdown", "-s", "-t", "100" });
             } catch (IOException e1) {
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
             }
         }
+    }
+
+    public void showDiwlog(String context) {// 跳出弹框
+        JDialog jdialog = new javax.swing.JDialog();
+        // 设置弹窗尺寸
+        jdialog.setSize(200, 150);
+        // 始终再最上面
+        jdialog.setAlwaysOnTop(true);
+        // 设置一直居中
+        jdialog.setLocationRelativeTo(null);
+        // 弹窗不关闭就无法操作下面的界面
+        jdialog.setModal(true);
+        // 设置描述,添加到弹窗里面
+        JLabel warn = new JLabel(context);
+        // 设置描述大小
+        warn.setBounds(0, 0, 200, 150);
+        // 将弹窗添加到弹窗容器内
+        jdialog.getContentPane().add(warn);
+        // 让弹窗显示
+        jdialog.setVisible(true);
     }
 
 }
