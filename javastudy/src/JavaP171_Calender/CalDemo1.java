1a53cfaae4cadbece479b0baadb5621b3c5a5053
vblaws <h3239498031@gmail.com>
2024-05-16 10:59:44 +0800

    t提交

---
+++ javastudy/src/JavaP171_Calender/CalDemo1.java
@@ -0,0 +1,11 @@
+package JavaP171_Calender;
+
+import java.util.Calendar;
+
+public class CalDemo1 {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTimeInMillis());
+
+    }
+}
