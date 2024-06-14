702a15b283e2e49abf67fe35f9acc7c9bc706d80
vblaws <h3239498031@gmail.com>
2024-06-14 10:30:13 +0800

    学习

--- javastudy/src/常见算法/常见算法的API/lambda表达式/LambdaDemo.java
+++ javastudy/src/常见算法/常见算法的API/lambda表达式/LambdaDemo.java
@@ -4,22 +4,35 @@ import java.util.*;
 
 public class LambdaDemo {
     public static void main(String[] args) {
+        // 原来的方式
+        // 调用一个方法,如果形参是一个接口,那么我们需要传入这个接口的实现类对象
+        // 如果方法只需要调用一次,那么可以使用匿名内部类
+        method(
+                new Swim() {
+                    @Override
+                    public void Swimimng() {
+                        System.out.println("我在匿名内部类里面写");
+                    }
+                });
 
-        Integer[] arr = { 1, 2, 3, 1, 22, 2, 22, 1, 111, 112 };
-        Arrays.sort(arr, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
-            }
+        // 利用Lambda表达式的方式
+        method(() -> {
+            System.out.println("我在Lambda里面写");
         });
-        // lambda表达式
-        Arrays.sort(arr, (Integer o1, Integer o2) -> {
-            return o2 - o1;
-        });
-        Arrays.sort(arr, (o1, o2) -> o2 - o1);
-
-        System.out.println(Arrays.toString(arr));
-        // 上面一段的简单理解:
-        // o1-o2升序 // o2-o1降序
     }
+
+    public static void method(Swim s) {
+        s.Swimimng();
+    }
+}
+
+/**
+ * Swim
+ * 
+ */
+// 函数式接口:有且只有一个抽象方法的接口
+
+@FunctionalInterface
+interface Swim {
+    void Swimimng();
 }
